1ed31b3c504bf4c2876b4facf022e5106c4feea5
Yifan Liu <yifan1.liu@intel.com>
2026-02-05 11:39:44 +0800

    acrn: Rewrite apic ID mapping logic
    Previous code is confusing and buggy. Rewrite it.
    
    Signed-off-by: Yifan Liu <yifan1.liu@intel.com>

--- src/acrn/acrn_driver.c
+++ src/acrn/acrn_driver.c
@@ -2535,109 +2535,40 @@ error:
     virObjectUnref(caps);
     return NULL;
 }
-static int
-acrnGetPorcessor(char *str) {
-        int processor;
-        char *start = strstr(str, "processor");
-        if (start != NULL) {
-                sscanf(start, "processor\t: %u", &processor);
-                return processor;
-        }
-        return -1;
-}
-static int
-acrnGetApiced(char *str) {
-        int apicid;
-        char *start = strstr(str, "apicid");
-        if (start != NULL) {
-                sscanf(start, "apicid\t\t: %d", &apicid);
-                return apicid;
-        }
-        return -1;
-}
 
 static int
 acrnGetLapicMap(int nprocs, unsigned int **apicidMap)
 {
-#define READ_LEN (128)
-    int fd = -1, ret = 0, i;
-    int pos = 0, len = READ_LEN, total = 0;
-    int processor = -1, apicid = -1;
-    char str[READ_LEN], *token, *next, s[2] = {0xa, 0x0};
-    ssize_t rc;
-    unsigned int *map;
+	char line[512];
+	int processor = 0, apicid = -1, ret = 0;
+	unsigned int *map;
+	FILE *f;
 
-    if (VIR_ALLOC_N(map, nprocs) < 0) {
-        ret = -ENOMEM;
-        goto cleanup;
-    }
+	f = fopen(CPUINFO_PATH, "r");
+	if (f == NULL) {
+		virReportError(VIR_ERR_OPEN_FAILED, _("%s"), CPUINFO_PATH);
+		return -1;
+	}
 
-    if ((fd = open(CPUINFO_PATH, O_RDONLY)) < 0) {
-            virReportError(VIR_ERR_OPEN_FAILED, _("%s"), CPUINFO_PATH);
-	    ret = -1;
-        goto cleanup;
-    }
+	if (VIR_ALLOC_N(map, nprocs) < 0) {
+		ret = -ENOMEM;
+		goto cleanup;
+	}
 
-    rc = pread(fd, &str[pos], len, 0);
-    do {
-        total += rc;
-        token = strtok(str, s);
+	while (fgets(line, sizeof(line), f)) {
+		if (!STRPREFIX(line, "apicid"))
+			continue;
+		if (sscanf(line, "apicid : %u", &apicid) <= 0)
+			continue;
+		VIR_DEBUG("APICID mapping: %d : %d", processor, apicid);
+		map[processor++] = apicid;
+	}
 
-        if(processor != -1) {
-            apicid = acrnGetApiced(str);
-        } else {
-            processor = acrnGetPorcessor(str);
-            if (processor >= nprocs)
-                processor = -1;
-        }
-
-        if(processor != -1 && apicid != -1) {
-                map[processor] = apicid;
-                processor = -1;
-                apicid = -1;
-        }
-
-        next = strtok(NULL, s);
-        do {
-            if(processor != -1) {
-                apicid = acrnGetApiced(token);
-            } else {
-                processor = acrnGetPorcessor(token);
-                if (processor >= nprocs)
-                    processor = -1;
-            }
-
-            if(processor != -1 && apicid != -1) {
-                map[processor] = apicid;
-                processor = -1;
-                apicid = -1;
-            }
-            token = next;
-        } while ((next = strtok(NULL, s)) != NULL);
-
-        pos = 0;
-        for (i = len - 1; i > 1; i--) {
-            if (str[i - 1] == 0x0 || str[i-1] == 0x20) {
-                pos = len - i - 1;
-                if (pos != 0) {
-                    memmove(str, &str[i], pos);
-                }
-                len = READ_LEN - pos;
-                break;
-            }
-        }
-    } while ((rc = pread(fd, &str[pos], len, total)) > 0);
-
-    *apicidMap = map;
+	*apicidMap = map;
 
 cleanup:
-    if (fd > 0)
-        close(fd);
-
-    if (ret != 0 && map != NULL) {
-        VIR_FREE(map);
-    }
-    return ret;
+	fclose(f);
+	return ret;
 }
 
 /*
