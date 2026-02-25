1983f2d64e77feff0901b8e17a8591bf6842f17e
Yifan Liu <yifan1.liu@intel.com>
2026-02-25 11:28:50 +0000

    acrn: Automatically manage driver upon pci device passthrough
    Bind passthrough PCI device to pci-stub and re-bind to original driver
    when VM is shutdown.
    
    Signed-off-by: Yifan Liu <yifan1.liu@intel.com>

--- src/acrn/acrn_process.c
+++ src/acrn/acrn_process.c
@@ -287,6 +287,37 @@ acrnVMIsLapicPT(virDomainObj *vm G_GNUC_UNUSED)
     return false;
 }
 
+static int
+acrnHostdevPrepareDomainDevices(acrnConn *driver, virDomainDef *def, unsigned int flags)
+{
+    ssize_t i;
+
+    for (i = 0; i < def->nhostdevs; i++) {
+        virDomainHostdevDef *hostdev = def->hostdevs[i];
+        virDomainHostdevSubsysPCI *pcisrc = &hostdev->source.subsys.u.pci;
+
+        if (hostdev->mode == VIR_DOMAIN_HOSTDEV_MODE_SUBSYS &&
+            hostdev->source.subsys.type == VIR_DOMAIN_HOSTDEV_SUBSYS_TYPE_PCI &&
+            pcisrc->driver.name == VIR_DEVICE_HOSTDEV_PCI_DRIVER_NAME_DEFAULT) {
+            pcisrc->driver.name = VIR_DEVICE_HOSTDEV_PCI_DRIVER_NAME_KVM;
+        }
+
+    }
+
+    /* Currently we prepare only PCI device */
+    return virHostdevPreparePCIDevices(driver->hostdevMgr, "acrn",
+            def->name, def->uuid, def->hostdevs, def->nhostdevs, flags);
+}
+
+static void
+acrnHostdevReAttachDomainDevices(acrnConn *driver,
+                                 const char *name,
+                                 virDomainHostdevDef **hostdevs,
+                                 int nhostdevs)
+{
+    virHostdevReAttachPCIDevices(driver->hostdevMgr, "acrn", name, hostdevs, nhostdevs);
+}
+
 int
 acrnProcessPrepareDomain(acrnConn *driver,
                           virDomainObj *vm,
@@ -297,6 +328,9 @@ acrnProcessPrepareDomain(acrnConn *driver,
     if (acrnVMIsLapicPT(vm)) {
         ret = acrnOfflineCPUs(driver, vm);
     }
+
+    if (acrnHostdevPrepareDomainDevices(driver, vm->def, 0) < 0)
+        ret = -1;
 
     return ret;
 }
@@ -345,6 +379,10 @@ virAcrnProcessStopCallback(struct _acrnConn *driver,
                      vm->def->name);
         }
     }
+
+    /* Passthrough device re-attach */
+    acrnHostdevReAttachDomainDevices(driver, vm->def->name, vm->def->hostdevs,
+            vm->def->nhostdevs);
 
     virCloseCallbacksDomainRemove(vm, NULL, acrnProcessAutoDestroy);
 
